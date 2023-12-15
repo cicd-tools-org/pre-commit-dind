565a10b6fd74d5115e586d37f97ca07ec8f57558
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-12-15 03:33:59 -0500

    feat(INSTALLER): patch pre-commit on install

--- pre_commit_dind/__main__.py
+++ pre_commit_dind/__main__.py
@@ -2,15 +2,30 @@ from __future__ import annotations
 
 import os
 import site
+import sys
 
 
-def install() -> None:
-    """Installs pre-commit with DinD support with cgroups version 2."""
-    target = os.path.join(
-        site.getsitepackages()[
-            0
-        ], 'pre_commit', '__main__.py',
+def installer() -> None:
+    """Patches pre-commit with pre-commit-dind."""
+
+    target1 = os.path.join(
+        site.getsitepackages()[0],
+        'pre_commit',
+        '__main__.py',
     )
+    target2 = os.path.join(
+        os.path.dirname(sys.executable),
+        'pre-commit',
+    )
+
+    _rewrite_file(target1)
+    _rewrite_file(target2)
+
+    print('pre-commit-dind was installed successfully!')
+
+
+def _rewrite_file(target: str) -> None:
+    """Rewrites the file at the given target to use pre-commit-dind."""
 
     with open(target, encoding='utf-8') as f:
         content = f.read()
@@ -19,12 +34,10 @@ def install() -> None:
         f.write(
             content.replace(
                 'from pre_commit.main import main',
-                'from pre_commit_dind.main import main',
+                'from pre_commit_dind import main',
             ),
         )
 
-    print('pre-commit-dind was installed successfully!')
-
 
 if __name__ == '__main__':
-    install()
+    installer()
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,32 +1,75 @@
 """Test the __main__ module."""
 from __future__ import annotations
 
+import os
 from io import BufferedIOBase
 from unittest import mock
 
 from pre_commit_dind import __main__
-from pre_commit_dind.__main__ import install
+from pre_commit_dind.__main__ import installer
 
 
 class TestMonkeyPatch:
     """Test the __main__ module."""
 
-    def test_install(self):
-        """Test the install function."""
+    def create_read_mock(self):
+        mock_obj = mock.MagicMock(spec=BufferedIOBase)
+        mock_obj.__enter__.return_value.read.return_value = \
+            'from pre_commit.main import main'
+        return mock_obj
+
+    def create_write_mock(self):
+        mock_obj = mock.MagicMock(spec=BufferedIOBase)
+        return mock_obj
+
+    def test_installer_file_operations(self):
+        """Test the installer function."""
 
         with mock.patch.object(__main__, 'open') as m_open:
-            read_mock = mock.MagicMock(spec=BufferedIOBase)
-            read_mock.__enter__.return_value.read.return_value = \
-                'from pre_commit.main import main'
-            write_mock = mock.MagicMock(spec=BufferedIOBase)
+            read_mock1 = self.create_read_mock()
+            write_mock1 = self.create_write_mock()
+            read_mock2 = self.create_read_mock()
+            write_mock2 = self.create_write_mock()
             m_open.side_effect = [
-                read_mock,
-                write_mock,
+                read_mock1,
+                write_mock1,
+                read_mock2,
+                write_mock2,
             ]
 
-            install()
+            installer()
 
-        read_mock.__enter__.return_value.read.assert_called_once()
-        write_mock.__enter__.return_value.write.assert_called_once_with(
-            'from pre_commit_dind.main import main',
+        read_mock1.__enter__.return_value.read.assert_called_once_with()
+        write_mock1.__enter__.return_value.write.assert_called_once_with(
+            'from pre_commit_dind import main',
         )
+        read_mock2.__enter__.return_value.read.assert_called_once_with()
+        write_mock2.__enter__.return_value.write.assert_called_once_with(
+            'from pre_commit_dind import main',
+        )
+
+    def test_installer_file_names(self):
+        """Test the installer function."""
+        mock_site_packages = 'mock/path1/dist-packages'
+        mock_executable = 'mock/path2/bin'
+        with (
+            mock.patch.object(
+                __main__.site, 'getsitepackages',
+            ) as m_packages,
+            mock.patch.object(
+                __main__.sys, 'executable',
+                os.path.join(mock_executable, 'python'),
+            ),
+            mock.patch.object(
+                __main__,
+                '_rewrite_file',
+            ) as m_rewrite,
+        ):
+            m_packages.return_value = [mock_site_packages]
+
+            installer()
+
+        assert m_rewrite.mock_calls == [
+            mock.call(f'{mock_site_packages}/pre_commit/__main__.py'),
+            mock.call(f'{mock_executable}/pre-commit'),
+        ]
